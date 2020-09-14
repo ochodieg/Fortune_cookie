e39d68693a7bbb0b564d356b0bab3632084df4c8
Ivan <the>
2020-09-13 23:38:53 -0600

    Add project files.

---
+++ Fortune_Cookie.vb
@@ -0,0 +1,63 @@
+﻿'D. Ivan Ochoa
+'RCET0265
+'Fall 2020
+'Multiplication_Table
+'https://github.com/ochodieg/Multiplication_Table
+
+
+Option Strict On
+Option Compare Text
+Option Explicit On
+
+
+Module Fortune_Cookie
+
+    Sub Main()
+        Dim randomNumber As Integer
+        Randomize()
+        Do
+
+            Console.WriteLine("Press Enter for ~FORTUNE~.")
+
+            Console.ReadLine()
+
+
+            randomNumber = GetRandomNumber(1, 2)
+
+            If randomNumber = 1 Then
+
+                Console.WriteLine("Your fortune.....YOU WILL PASS")
+
+            ElseIf randomNumber = 2 Then
+
+                Console.WriteLine("Your fortune.....YOU SHALL NOT PASS")
+
+            ElseIf randomNumber = 3 Then
+
+                Console.WriteLine("Avoid high calorie foods...... don't you love when fortune cookies are really just 
+advice cookies?")
+
+            End If
+
+            Console.ReadLine()
+
+            Console.Clear()
+
+        Loop
+
+
+
+
+    End Sub
+
+    Function GetRandomNumber(ByVal minimum As Integer, ByVal maximum As Integer) As Integer
+
+        Dim value As Single
+
+        value = ((maximum - minimum + 1) * Rnd()) + minimum
+
+        Return CInt(value)
+
+    End Function
+
+End Module
